e362501e20c752eca9dfa1ef4ad573a55077de63
sasivarman-s <sureshraman2680@gmail.com>
2026-07-30 13:53:01 +0530

    updated push

--- pages/data.js
+++ pages/data.js
@@ -5,19 +5,18 @@ btn.addEventListener("click", (e)=>{
     var phnumber = document.getElementById("phnumber")
     var mail = document.getElementById("mail")
     var msg = document.getElementById("msg")
-    var btndiv = document.getElementById("btndiv")
+    var end = document.getElementById("end")
     console.log(name.value)
     console.log(phnumber.value)
     console.log(mail.value)
     console.log(msg.value)
-    btn.innerHTML = "Submitted successfully.I'll contact you ASAP..!"
-    btn.style.backgroundColor = "grey"
-    btn.style.opacity = "0.5"
-    var tq = document.createElement("h3")
-    tq.style.fontSize = "15px"
-    tq.style.padding = "5px"
-    tq.innerHTML = "Thankyou for contacting❤️"
-    btndiv.appendChild(tq)
+    btn.remove("btn")
+    end.style.backgroundColor = "gray"
+    end.style.color = "white"
+    end.style.padding = "10px"
+    end.style.fontSize = "20px"
+    end.style.borderRadius = "30px"
+    end.innerHTML = "Submitted successfully, I'll contact you ASAP!..."
 })
 
 
